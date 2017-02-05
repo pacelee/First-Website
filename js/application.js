5b51051209f56d07c4ed55c25d73586b9fb0d048
pacelee <pacemlee@gmail.com>
2017-02-05 13:30:46 +0800

    Added javascript to experiments page

--- js/application.js
+++ js/application.js
@@ -1,5 +1,38 @@
-$(document).ready(function() {
-  $(".thumbnail").on("click" (function() {
-        $(this).hide(1000);
-  });
+
+$(function() {
+
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1500;
+    var pause = 5000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
 });
